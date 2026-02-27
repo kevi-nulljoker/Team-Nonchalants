68c03099e5f3004e420dd6da4022d1003c43b7f0
arjune4dev <arjunmishrae4@gmail.com>
2026-02-28 05:26:54 +0530

    --

--- FinSight/src/pages/Home.jsx
+++ FinSight/src/pages/Home.jsx
@@ -2,22 +2,15 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import AuthPage from "./SignUp";
 import Transactions from "./Transactions";
+import HeroSection from "../components/HeroSection";
 
 
 function Home() {
-  const { isAuthenticated, loading } = useContext(AuthContext);
-
-  if (loading) {
-    return null;
-  }
-
-  if (!isAuthenticated) {
-    return <AuthPage />;
-  }
 
   return (
     <>
-      <Transactions />
+      {/* <Transactions /> */}
+      <HeroSection />
     </>
   );
 }
